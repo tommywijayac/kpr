307fbe92cecee1e5eb73fbf26a84de2f97360a42
tommywijayac <tommy.wijaya.c@gmail.com>
2024-08-29 08:39:03 +0700

    simplify

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/tommywijayac/kpr
 
-go 1.23.0
+go 1.23
 
 require github.com/leekchan/accounting v1.0.0
 
--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"github.com/tommywijayac/kpr/internal"
+	"fmt"
+	"math"
+
+	"github.com/leekchan/accounting"
 )
 
+var ac accounting.Accounting
+
 func main() {
+	fmt.Println("hello from wasm")
+
+	ac = accounting.Accounting{Symbol: "IDR ", Precision: 2}
+
 	// hardcoded inputs
 	pokok := float64(1296000000)
 	bungafix := []float64{2.79, 5.79, 8.1, 10.1}
@@ -20,16 +29,40 @@ func main() {
 		panic("invalid period")
 	}
 
-	internal := internal.New()
-
 	// calculate tiered fix
 	for i := 0; i < len(periodefix); i++ {
-		pokok = internal.Calculate(pokok, bungafix[i], periodefix[i], periodetotal) // update pokok with remaining
+		pokok = calculate(pokok, bungafix[i], periodefix[i], periodetotal) // update pokok with remaining
 
 		// update values for next tiered fix
 		periodetotal = periodetotal - periodefix[i]
 	}
 
 	// calculate remaining as floating
-	internal.Calculate(pokok, float64(bungafloat), periodefloat, periodetotal)
+	calculate(pokok, float64(bungafloat), periodefloat, periodetotal)
+}
+
+func calculate(principal, interestRate float64, interestPeriod, totalPeriod int) float64 {
+	var (
+		monthlyInterestRate float64 = float64(interestRate) / 100 / 12
+
+		// still don't fully understand, but this makes principal is paid in certain percentage to interest & installment
+		loan        float64 = 1 + monthlyInterestRate
+		totalLoan   float64 = math.Pow(loan, float64(totalPeriod))
+		installment float64 = 1 - 1/totalLoan
+
+		interestInstallment float64 = principal * monthlyInterestRate   // angsuran bunga
+		monthlyInstallment  float64 = interestInstallment / installment // by dividing with (1 - 1/totalLoan), installment will contain (interestInstallment + principalInstallment)
+	)
+
+	for i := 0; i < interestPeriod; i++ {
+		interestInstallment = principal * monthlyInterestRate            // angsuran bunga
+		principalInstallment := monthlyInstallment - interestInstallment // angsuran pokok
+
+		fmt.Printf("[%d] cicilan: %v, bunga: %v, pokok: %v\n",
+			i+1, ac.FormatMoney(monthlyInstallment), ac.FormatMoney(interestInstallment), ac.FormatMoney(principalInstallment))
+
+		principal -= principalInstallment
+	}
+
+	return principal // return any remainder
 }
